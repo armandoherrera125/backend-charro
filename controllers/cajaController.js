b91b7560ef2569b141c38fda562fc867732202e9
armandoherrera125 <armando_125@hotmail.com>
2023-02-27 07:45:07 -0600

    Hora arreglada en caja

--- controllers/cajaController.js
+++ controllers/cajaController.js
@@ -2,14 +2,22 @@ const { request, response } = require("express");
 const { sequelize } = require("../db/connection");
 const { getAmountOfBox } = require("../helpers/CountProducts");
 const { Box } = require("../models/box");
+const moment = require("moment/moment");
 
 const cajaCreate = async (req = request, res = response) => {
     const { caja } = req.body;
     const amountOfBox = await getAmountOfBox();
     const id = amountOfBox + 1;
+
+    const time = moment.duration("06:00:00");
+    var date = moment();
+    date.subtract(time).format();
+    console.log(date.format());
+
+
     const today = new Date();
-    const createdAt = today.toLocaleString();
-    const updatedAt = today.toLocaleString();
+    const createdAt = date.format();
+    const updatedAt = date.format();
     try {
         const creatingBox = await Box.create({
             id,
