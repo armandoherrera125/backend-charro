bc96cd717ffb43776d52c07e598297452ff16e58
ArmandoHerrera <54688113+armandoherrera125@users.noreply.github.com>
2024-01-29 14:44:37 -0600

    Update new database connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize('mysql://root:gOoN3LrneqeMaqTnwwTN@containers-us-west-20.railway.app:5938/railway');
+const sequelize = new Sequelize('mysql://root:2gECB1AbeHG4-4CdEe1d24A6C2AAhAhg@monorail.proxy.rlwy.net:22247/railway');
 
 module.exports = {
     sequelize
