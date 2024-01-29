d67b72657e71684a76e3bca1fa29c5e0e77f6fcf
ArmandoHerrera <54688113+armandoherrera125@users.noreply.github.com>
2024-01-29 14:43:02 -0600

    Update probando connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT,
-    define: {
-        timestamps: false
-    }
-});
+const sequelize = new Sequelize('mysql://root:gOoN3LrneqeMaqTnwwTN@containers-us-west-20.railway.app:5938/railway');
 
 module.exports = {
     sequelize
